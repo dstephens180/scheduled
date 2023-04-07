99b48da10f21386f1dc9cc79bcf574cd4533bae4
dstephens180 <dstephens@tnsinc.com>
2023-04-07 15:25:45 -0400

    Including render for geomarket listing id details

--- render_rmd.R
+++ render_rmd.R
@@ -9,3 +9,11 @@ rmarkdown::render(
   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
 )
 
+
+# # Geomarket Listing Id Details from rented_art
+# Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+# rmarkdown::render(
+#   input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_geomarket_full_comp_details.Rmd",
+#   output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_geomarket_full_comp_details.html",
+#   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+# )
