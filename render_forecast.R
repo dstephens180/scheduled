c157bbac386c69b377687101e2fdcf0c9214a087
dstephens180 <dstephens@tnsinc.com>
2023-04-25 15:52:08 -0400

    including year 2 nested forecast in render file.

--- render_forecast.R
+++ render_forecast.R
@@ -1,10 +1,19 @@
 library(rmarkdown)
 
 
-# Nested Forecast from rentedrmsprod db
+# Nested Forecast Year 1, from rentedrmsprod db
 Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
 rmarkdown::render(
-  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_forecast.Rmd",
-  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_forecast.html",
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_forecast_year_1.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_forecast_year_1.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
+# Nested Forecast Year 2, from rentedrmsprod db
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_forecast_year_2.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_forecast_year_2.html",
   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
 )
