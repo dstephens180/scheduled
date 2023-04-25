946af81518869797541e53bfec17508216cc7e78
dstephens180 <dstephens@tnsinc.com>
2023-04-25 15:20:29 -0400

    Render naming update.

--- render_rmd.R
+++ render_rmd.R
@@ -4,16 +4,27 @@ library(rmarkdown)
 # Art Listing Id's from Production DB
 Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
 rmarkdown::render(
-  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_data_pull.Rmd",
-  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_data_pull.html",
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_synced_only_data_pull.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_synced_only_data_pull.html",
   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
 )
 
 
-# # Geomarket Listing Id Details from rented_art
-# Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
-# rmarkdown::render(
-#   input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_geomarket_full_comp_details.Rmd",
-#   output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_geomarket_full_comp_details.html",
-#   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
-# )
+# Art Groups from Production DB
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_art_groups.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rentedrmsprod_art_groups.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
+# Geomarket Listing Id Details from rented_art
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rented_art_geomarket_full_comp_details.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_rented_art_geomarket_full_comp_details.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
