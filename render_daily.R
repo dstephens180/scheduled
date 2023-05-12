40567f0d9dcac8e0b798a5a795f79f29b62f6f49
dstephens180 <dstephens@tnsinc.com>
2023-05-12 17:35:07 -0400

    Daily rendering for tableau extracts, feature engineering and churn & network analysis.

---
+++ render_daily.R
@@ -0,0 +1,38 @@
+library(rmarkdown)
+library(rsconnect)
+
+
+# Tableau extract via python
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_tableau_data_extract_via_python.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_tableau_data_extract_via_python.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
+# ARR Revenue v2 - Feature Engineering
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_feature_engineering_arr_revenue_v2.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_feature_engineering_arr_revenue_v2.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
+# TNS Churn Analysis
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\01_tns_customer_churn_analysis.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\01_tns_customer_churn_analysis.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
+# TNS Customer Network Analysis
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\02_tns_customer_network_analysis.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\02_tns_customer_network_analysis.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
