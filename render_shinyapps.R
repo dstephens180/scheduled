c753c514560db5afb68b494cbc647d6456d0ebd3
dstephens180 <dstephens@tnsinc.com>
2023-05-15 11:17:20 -0400

    Including Shiny Model Focus app.

--- render_shinyapps.R
+++ render_shinyapps.R
@@ -9,7 +9,8 @@ rsconnect::setAccountInfo(
 
 
 
-# upload Customer Network Analysis
+# --
+# Customer Network/Segment Analysis
 rsconnect::deployApp(
   appDir = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn",
   appName = "tns_customer_networks_qYoPP1O2yVYyeTjJ6ay64D9qcurzYu37",
@@ -19,11 +20,13 @@ rsconnect::deployApp(
                '00_data/tns_customer_network_tbl_graph.rds'),
   server = 'shinyapps.io',
   forceUpdate = T,
-  launch.browser = F)
+  launch.browser = F,
+  account = 'msv-analytics')
 
 
 
-# upload Survey Monkey Sentiment Analysis
+# --
+# Survey Monkey Sentiment Analysis
 rsconnect::deployApp(
   appDir = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn",
   appName = "survey_monkey_sentiment_analysis_0i7xueHbqT0YxSmcKZNCqKWsr3",
@@ -33,4 +36,23 @@ rsconnect::deployApp(
                '00_data/survey_monkey_sentiment_by_account_tbl.rds'),
   server = 'shinyapps.io',
   forceUpdate = T,
-  launch.browser = F)
+  launch.browser = F,
+  account = 'msv-analytics')
+
+
+
+# --
+# Shiny Model Focus
+rsconnect::deployApp(
+  appDir = "C:\\Users\\DavidStephens\\Desktop\\Github\\artr_replica\\shiny",
+  appName = "shiny_model_focus_5vrvyRo5mARtetyCGWdFkcKCUzxB2Yde",
+  appFiles = c('shiny_model_focus.Rmd',
+               'www/Rented-logo-small.png',
+               'config.yml',
+               'art_groups_cleaned.rds',
+               'art_listing_ids_cleaned.parquet',
+               'comps_full_geomarket_details_tbl.parquet'),
+  server = 'shinyapps.io',
+  forceUpdate = T,
+  launch.browser = F,
+  account = 'msv-analytics')
