321d3287d1e71a263f4c6942e6d9a7138e9b784c
dstephens180 <dstephens@tnsinc.com>
2023-05-18 10:46:26 -0400

    including package prefixes to each function.

--- render_shinyapps.R
+++ render_shinyapps.R
@@ -1,16 +1,22 @@
 library(rmarkdown)
 library(rsconnect)
+library(blastula)
+library(tidyverse)
+
+date_time <- blastula::add_readable_time()
 
 
+
+
+
+###################
+# Customer Network/Segment Analysis
 rsconnect::setAccountInfo(
   name='msv-analytics',
   token='5A59317284D8C8A170ED06710E4C3367',
   secret='7+tvar0BSbNFdloTnP7cnnI8XdmZhY9Ke+s+adPv')
 
 
-
-# --
-# Customer Network/Segment Analysis
 rsconnect::deployApp(
   appDir = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn",
   appName = "tns_customer_networks_qYoPP1O2yVYyeTjJ6ay64D9qcurzYu37",
@@ -24,9 +30,32 @@ rsconnect::deployApp(
   account = 'msv-analytics')
 
 
+body_text <-
+  blastula::md(stringr::str_glue("Good news! The **Customer Network/Segment Analysis App** was successfully deployed to shinyapps.io on {date_time}."))
 
-# --
+email <- blastula::compose_email(body = body_text)
+
+
+# send the email
+email %>%
+  blastula::smtp_send(
+    from = "dstephens@tnsinc.com",
+    to = "dstephens@tnsinc.com",
+    subject = stringr::str_glue("Customer Segmentation Analysis"),
+    credentials = blastula::creds_file(file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\gmail_creds"))
+
+
+
+
+
+###################
 # Survey Monkey Sentiment Analysis
+rsconnect::setAccountInfo(
+  name='msv-analytics',
+  token='5A59317284D8C8A170ED06710E4C3367',
+  secret='7+tvar0BSbNFdloTnP7cnnI8XdmZhY9Ke+s+adPv')
+
+
 rsconnect::deployApp(
   appDir = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn",
   appName = "survey_monkey_sentiment_analysis_0i7xueHbqT0YxSmcKZNCqKWsr3",
@@ -40,9 +69,32 @@ rsconnect::deployApp(
   account = 'msv-analytics')
 
 
+body_text <-
+  blastula::md(stringr::str_glue("Good news! The **Survey Monkey Sentiment Analysis** was successfully deployed to shinyapps.io on {date_time}."))
 
-# --
+email <- blastula::compose_email(body = body_text)
+
+
+# send the email
+email %>%
+  blastula::smtp_send(
+    from = "dstephens@tnsinc.com",
+    to = "dstephens@tnsinc.com",
+    subject = stringr::str_glue("Survey Monkey Sentiment App"),
+    credentials = blastula::creds_file(file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\gmail_creds"))
+
+
+
+
+
+###################
 # Shiny Model Focus
+rsconnect::setAccountInfo(
+  name='msv-analytics',
+  token='5A59317284D8C8A170ED06710E4C3367',
+  secret='7+tvar0BSbNFdloTnP7cnnI8XdmZhY9Ke+s+adPv')
+
+
 rsconnect::deployApp(
   appDir = "C:\\Users\\DavidStephens\\Desktop\\Github\\artr_replica\\shiny",
   appName = "shiny_model_focus_5vrvyRo5mARtetyCGWdFkcKCUzxB2Yde",
@@ -56,3 +108,22 @@ rsconnect::deployApp(
   forceUpdate = T,
   launch.browser = F,
   account = 'msv-analytics')
+
+
+body_text <-
+  blastula::md(stringr::str_glue("Good news! The **Shiny Model Focus App** was successfully deployed to shinyapps.io on {date_time}."))
+
+email <- blastula::compose_email(body = body_text)
+
+
+# send the email
+email %>%
+  blastula::smtp_send(
+    from = "dstephens@tnsinc.com",
+    to = "dstephens@tnsinc.com",
+    subject = stringr::str_glue("Shiny Model Focus App"),
+    credentials = blastula::creds_file(file = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\gmail_creds"))
+
+
+
+
