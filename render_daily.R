4c4e3d085bf5ee951c299c3ebba5c20d7fa23213
dstephens180 <dstephens@tnsinc.com>
2023-07-25 08:47:44 -0400

    Including Rollup Metrics feature engineering and PredictHQ API pull.

--- render_daily.R
+++ render_daily.R
@@ -2,11 +2,20 @@ library(rmarkdown)
 library(rsconnect)
 
 
-# ARR Revenue v2 - Feature Engineering
+# Feature Engineering: ARR Revenue v2
 Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
 rmarkdown::render(
   input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_feature_engineering_arr_revenue_v2.Rmd",
   output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_feature_engineering_arr_revenue_v2.html",
+  output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+)
+
+
+# Feature Engineering: Survey Monkey Rollup Metrics
+Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+rmarkdown::render(
+  input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_feature_engineering_survey_monkey_rollup.Rmd",
+  output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\00_feature_engineering_survey_monkey_rollup.html",
   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
 )
 
@@ -36,3 +45,12 @@ rmarkdown::render(
   output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\customer_churn\\01_geospatial_spend_by_state.html",
   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
 )
+
+
+# PredictHQ API Pull
+# Sys.setenv(RSTUDIO_PANDOC = "C:\\Program Files\\Pandoc")
+# rmarkdown::render(
+#   input       = "C:\\Users\\DavidStephens\\Desktop\\Github\\predicthq-streamlit\\01_api_connection.Rmd",
+#   output_file = "C:\\Users\\DavidStephens\\Desktop\\Github\\predicthq-streamlit\\01_api_connection.html",
+#   output_dir  = "C:\\Users\\DavidStephens\\Desktop\\Github\\scheduled\\00_archive"
+# )
